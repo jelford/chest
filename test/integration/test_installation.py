a6a3867c0f56464a11589db15a64361e7e19ad76
James Elford <james.p.elford@gmail.com>
2014-04-05 14:47:57 +0100

    added basic test infrastructure

--- test/integration/test_installation.py
+++ test/integration/test_installation.py
@@ -5,16 +5,25 @@ import os
 import shutil
 import subprocess
 
+import unittest
+
+_project_root_dir = os.getcwd()
+
 def install_project(destination_directory, project_root_dir):
     subprocess.check_call(['virtualenv', destination_directory])
     installation_command = '. bin/activate; pip install {project_root_dir}'.format(**locals())
     subprocess.check_call(installation_command, shell=True, cwd=destination_directory)
 
+class TestInstallation(unittest.TestCase):
+    def setUp(self):
+        self.project_root_dir = _project_root_dir
+        self.temp_dir = mkdtemp('_chest')
+
+    def test_installation(self):
+        install_project(self.temp_dir, self.project_root_dir)
+        
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
 
 if __name__ == '__main__':
-    temp_dir = mkdtemp('_chest')
-    try:
-        project_root_directory = os.getcwd()
-        install_project(temp_dir, project_root_directory)
-    finally:
-        shutil.rmtree(temp_dir)
+    unittest.main()
