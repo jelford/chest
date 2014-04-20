4f5c8b12e3e8f24c54c325d0bdbef5cf982ebeac
James Elford <james.p.elford@gmail.com>
2014-04-20 16:58:12 +0100

    js tidyup plus make the server 404 when receiving requests for unknown data

--- src/chest/start.py
+++ src/chest/start.py
@@ -11,13 +11,17 @@ from datetime import datetime
 
 template_loader = tornado.template.Loader(resource_filename(__name__, 'resources'))
 
-all_data = defaultdict(list)
+all_data = {}
 
 class DataEndpointHandler(tornado.web.RequestHandler):
     def post(self, series_name):
         data = json.loads(self.request.body)
         data.update({'_datetime_submitted': datetime.now().isoformat()})
-        all_data[series_name].append(data)
+        try:
+            current_data_points = all_data[series_name]
+        except KeyError:
+            current_data_points = all_data[series_name] = []
+        current_data_points.append(data)
         self.finish()
 
     def get(self, series_name):
@@ -33,7 +37,7 @@ class ChartEndpointHandler(tornado.web.RequestHandler):
         try:
             data = all_data[series_name]
         except KeyError:
-            self.response_code = 404
+            self.send_error(404)
             return
 
         self.write(
--- test/integration/test_data_retrieval.py
+++ test/integration/test_data_retrieval.py
@@ -10,8 +10,11 @@ class StoreAndRetrieveDataTestCase(RunningServerTestCase, HttpRequestMaker):
         self.assertEqual(self.post('series_name', {'value': 10}).status_code, 200)
         r = self.get('series_name')
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.json(), {u'd': [ {'value': 10} ] })
-
+        data_returned = r.json()
+        self.assertTrue(u'd' in data_returned)
+        points = data_returned[u'd']
+        self.assertEqual(len(points), 1)    
+        self.assertEqual(points[0][u'value'], 10)
 
 if __name__ == '__main__':
     unittest.main()
